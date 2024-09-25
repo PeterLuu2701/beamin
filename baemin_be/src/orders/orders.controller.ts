da07025b90e82c635304ab3a450b74c3fedc1ff9
PeterLuu2701 <longluuduchoanglong@gmail.com>
2024-09-25 19:06:15 +0700

    update

--- baemin_be/src/orders/orders.controller.ts
+++ baemin_be/src/orders/orders.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
 
+export class ordersType {
+  @ApiProperty()
+  user_id: number
+
+  @ApiProperty()
+  restaurant_food_id: number
+
+  @ApiProperty()
+  restaurant_id: number
+
+  @ApiProperty()
+  food_name: string
+
+  @ApiProperty()
+  restaurant_name: string
+
+  @ApiProperty()
+  price: number
+
+  @ApiProperty()
+  quantity: number
+
+  @ApiProperty()
+  total: number
+}
+
+@ApiTags('orders')
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @Post('create-order')
+  order(@Body() createOrdersDto: ordersType) {
+    return this.ordersService.order(createOrdersDto);
+  }
 }
--- baemin_be/src/orders/orders.service.ts
+++ baemin_be/src/orders/orders.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { ordersType } from './orders.controller';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  async order(createOrdersDto: ordersType) {
+    try {
+      const createOrder = await prisma.orders.create({
+        data: {
+            user_id: createOrdersDto.user_id,
+            restaurant_food_id: createOrdersDto.restaurant_food_id,
+            restaurant_id: createOrdersDto.restaurant_id,
+            food_name: createOrdersDto.food_name,
+            restaurant_name: createOrdersDto.restaurant_name,
+            price: createOrdersDto.price,
+            quantity: createOrdersDto.quantity,
+            total: createOrdersDto.price * createOrdersDto.quantity
+        },
+      });
+      return createOrder;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
